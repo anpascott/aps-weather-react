65b09f19ac142c1f17f8a320a727b2fe0fdb99f5
anpascott <anpascott@gmail.com>
2021-05-23 13:40:23 -0500

    fixed-bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import "./WeatherForecastDay.js";
 import axios from 'axios';
@@ -8,6 +8,11 @@ export default function WeatherForecast(props) {
 
   let [loaded, setLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(null);
+
+  useEffect (() => {
+   setLoaded(false);
+   }, [props.coordinates]);
+  
 
   function handleForecast (response) {
   setForecastData(response.data.daily);
